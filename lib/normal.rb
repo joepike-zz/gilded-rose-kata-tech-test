ee930c76c41605fc026f4100ea3e297bd9ffc53c
joepike <joseph.l.pike@gmail.com>
2018-01-31 11:24:53 +0000

    created normal class and test

--- lib/normal.rb
+++ lib/normal.rb
@@ -8,16 +8,13 @@ class Normal
 
   def update_quality
     @items.each do |item|
-      if quality_condition(item) and item.sell_in >= 0
-        item.quality - 1
-      elsif quality_condition and item.sell_in < 0
+      item.sell_in = item.sell_in - 1
+      if item.sell_in < 0
         item.quality - 2
+      else
+        item.quality - 1
       end
     end
   end
-
-  def quality_condition(item)
-    item.quality >= 0 and item.quality <= 50
-  end
-
+  
 end
